eea742d9bf925b668d0749bad3494ff6aaa62252
Marco Antonio Dutra <Marco Antonio Dutra>
2022-11-18 13:14:29 +0000

    gerador de recibo

---
+++ index.js
@@ -0,0 +1,14 @@
+function gerarRecibo() {
+    document.getElementById('recibo_emitente').innerHTML = document.getElementById('emitente').value;
+    document.getElementById('recibo_nome').innerHTML = document.getElementById('nome').value;
+    document.getElementById('recibo_valor').innerHTML = parseFloat(document.getElementById('valor').value).toLocaleString('pt-br', {minimumFractionDigits: 2});
+    document.getElementById('cpf_numero').innerHTML = (document.getElementById('cpf').value).toLocaleString('pt-br');
+    document.getElementById('recibo_data').innerHTML = dataAtual();
+    document.getElementById('cpf').innerHTML = parseFloat(document.getElementById('cpf').value).toLocaleString('pt-br');
+    window.print();
+    
+}
+    function dataAtual() {
+    let data = new Date();
+    return data.getDate() + "/" + (data.getMonth()+1) + "/" + data.getFullYear();
+}
